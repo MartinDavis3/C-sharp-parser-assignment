b41eb0940e553ee2f302b448ba5fcfe79cf898b0
Martin Davis <martin_davis3@yahoo.ca>
2020-05-10 10:42:45 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C__parser_assignment
+{
+  class Program
+  {
+    static void OutputInstructions()
+    {
+      Console.WriteLine("Input an option from 1 to 3:");
+      Console.WriteLine("1 to enter a filename.");
+      Console.WriteLine("2 to enter a string to be removed in the file.");
+      Console.WriteLine("3 to exit.");
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+  }
+}
